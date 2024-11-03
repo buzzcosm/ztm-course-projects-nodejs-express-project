b796b7ad99fdb0d8b3b01c43f453fe6cb6507b10
Evgueni Kounik <kounik.ch@gmail.com>
2024-11-03 03:16:44 +0100

    Add post friends

--- server.js
+++ server.js
@@ -15,11 +15,30 @@ const friends = [
   },
 ];
 
+// logging middleware
 app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
   console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: 'Missing friend name.',
+    });
+  }
+
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+  friends.push(newFriend);
+
+  res.status(201).json(newFriend);
 });
 
 app.get('/friends', (req, res) => {
@@ -33,7 +52,7 @@ app.get('/friends/:id', (req, res) => {
     res.status(200).json(friend);  
   } else {
     res.status(404).json({
-      error: "Friend does not exist",
+      error: 'Friend does not exist',
     });
   }
 });
