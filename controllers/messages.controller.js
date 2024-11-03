fde17a785dfc992b5e424d598260c4f604ed9975
Evgueni Kounik <kounik.ch@gmail.com>
2024-11-03 18:41:43 +0100

    Add sendFile functionality on use path of node

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 function getMessages(req, res) {
-  res.send('<ul><li>Helloo Albert!</li></ul>');
+  const imagePath = path.join(__dirname, '..', 'public', 'skimountain.jpg');
+  res.sendFile(imagePath);
+  // res.send('<ul><li>Helloo Albert!</li></ul>');
 };
 
 function postMessage(req, res) {
